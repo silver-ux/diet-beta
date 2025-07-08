d635cdfe3baec8e7efafec5ebe93e7c720a7d56c
Shota in lab <sh.studylab@gmail.com>
2025-07-08 16:27:12 +0900

    commit

--- app/api/user/signin/route.ts
+++ app/api/user/signin/route.ts
@@ -1,2 +1,11 @@
+import { createClient } from "@/supabase/server";
+import { NextRequest } from "next/server";
+
 // ログインデータをアップロード
-export async function POST() {}
+export async function POST(req: NextRequest) {
+  const supabase = await createClient();
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+}
