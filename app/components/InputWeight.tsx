ef508688ff346b726eb05c7df21d463a304104a4
Shota in lab <sh.studylab@gmail.com>
2025-07-15 13:37:51 +0900

    inputが変だったので、numberからtextに変えた

--- app/components/InputWeight.tsx
+++ app/components/InputWeight.tsx
@@ -4,14 +4,14 @@ import Container from "./Container";
 import { Props } from "../page";
 
 const InputWeight = ({ setWeight }: Props) => {
-  const [num, setNum] = useState<number>(50);
+  const [num, setNum] = useState<string>("");
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     const res = await fetch("/api/post", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
-        weight: num,
+        weight: Number(num),
       }),
     });
 
@@ -34,15 +34,16 @@ const InputWeight = ({ setWeight }: Props) => {
     <Container className={"mt-5"}>
       <form onSubmit={handleSubmit}>
         <input
-          type="number"
-          min={20}
+          type="text"
+          inputMode="numeric"
+          pattern="\d*"
           value={num}
-          onChange={(e) => setNum(Number(e.target.value))}
-          className="border-1 py-1.5 px-4 rounded-md"
+          onChange={(e) => setNum(e.target.value)}
+          className="w-full md:w-fit border-1 py-1.5 px-4 rounded-md mb-2"
         />
         <button
           type="submit"
-          className="border-1 rounded-md p-1 bg-red-100 ml-3 cursor-pointer hover:bg-red-200"
+          className="w-full md:w-fit border-1 rounded-md p-1 bg-red-100 md:ml-3 cursor-pointer hover:bg-red-200"
         >
           送信
         </button>
